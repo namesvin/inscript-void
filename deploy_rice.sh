285ecf9d4c9244bdfa0fe0bbb12da2b021b343cc
Beni <69584313+benilol98@users.noreply.github.com>
2023-01-24 10:48:11 +0100

    added st

--- deploy_rice.sh
+++ deploy_rice.sh
@@ -2,5 +2,9 @@
 sudo xbps-install -Syu git
 git clone https://github.com/namesvin/void-dots
 cd void-dots
+git clone https:///github.com/lukesmithxyz/st
+cd st
+sudo make clean install
+cd ..
 mv * ../
 # install everything thx uwu
