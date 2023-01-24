30af8172c29ca0cebebd9915791a2e0e64941b38
v <temporarylife666@gmail.com>
2023-01-24 08:42:42 +0100

    idk yet

--- void.sh
+++ void.sh
@@ -1,5 +1,3 @@
-DIR=$(pwd)
-
 echo "Give disk you want to install on! (/dev/sdX)"
 read DISK
 echo "What should your hostname be?"
@@ -9,15 +7,43 @@ read PASSWORD
 
 dd if=/dev/zero of=$DISK bs=1M count=4
 
-printf "g\nn\n\n\n+500M\nt\n1\nn\n\n\n\nw\n" | fdisk $DISK
+format_bios() {
+    drive=$1
+    fs=$2
+    size=$(parted -s /dev/sda unit MB print | head -n 2 | tail -n 1 | awk '{ print $3 }')
 
-mkfs.fat -F 32 ${DISK}1
-mkfs.xfs -f ${DISK}2
+    parted -s $drive mklabel msdos
+    parted -s $drive mkpart primary xfs 0 $size
+    parted -s $drive set 1 boot on
+}
 
-mkdir -p /mnt/void/
-mount "${DISK}2" /mnt/void
-mkdir -p /mnt/void/boot
-mount "${DISK}1" /mnt/void/boot
+format_uefi() {
+    drive=$1
+    fs=$2
+    size=$(parted -s /dev/sda unit MB print | head -n 2 | tail -n 1 | awk '{ print $3 }')
+
+    parted -s $drive mklabel gpt
+    parted -s $drive mkpart primary fat32 0 500
+    parted -s $drive set 1 esp on
+    parted -s $drive mkpart primary xfs 500 $size
+}
+
+dmesg | grep -q "EFI v"
+if [ $? -eq 0 ]
+then
+    format_uefi()
+    mkfs.fat -F 32 ${DISK}1
+    mkfs.xfs -f ${DISK}2
+    mkdir -p /mnt/void/
+    mount "${DISK}2" /mnt/void
+    mkdir -p /mnt/void/boot
+    mount "${DISK}1" /mnt/void/boot
+else
+    format_bios()
+    mkfs.xfs -f ${DISK}2
+    mkdir -p /mnt/void/
+    mount "${DISK}1" /mnt/void
+fi
 
 cd /mnt/void
 
@@ -43,7 +69,7 @@ dmesg | grep -q "EFI v"    # -q tell grep to output nothing
 if [ $? -eq 0 ]      # check exit code; if 0 EFI, else BIOS
 then
     echo "xbps-install -Suy xbps && xbps-install -uy && xbps-install -y base-system && xbps-remove -y base-voidstrap && xbps-install -y grub grub-x86_64-efi && grub-install --target=x86_64-efi --efi-directory=/boot && grub-mkconfig -o /boot/grub/grub.cfg && xbps-reconfigure -fa && passwd root" > /mnt/void/root/post.sh
-  else
+else
     echo "xbps-install -Suy xbps && xbps-install -uy && xbps-install -y base-system && xbps-remove -y base-voidstrap && xbps-install -y grub && grub-install /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg && xbps-reconfigure -fa && passwd root" > /mnt/void/root/post.sh
 fi
 
