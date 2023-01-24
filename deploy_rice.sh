4a0e437a1baed6a8b103d0560667b55a6fa86796
Beni <69584313+benilol98@users.noreply.github.com>
2023-01-24 10:54:05 +0100

    Update deploy_rice.sh

--- deploy_rice.sh
+++ deploy_rice.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # Should be ran as normal user!!!!!!!
 cd ~
-sudo xbps-install -Syu git
+git clone https://github.com/void-linux/void-packages.git
+cd void-packages
+./xbps-src binary-bootstrap
+echo XBPS_ALLOW_RESTRICTED=yes >> etc/conf
+./xbps-src pkg spotify
+./xbps-src pkg discord
+xbps-install --repository hostdir/binpkgs spotify discord
+sudo xbps-install -Syu git rofi i3 polybar xorg font-awesome5
+cd ~
 git clone https://github.com/namesvin/void-dots
 cd void-dots
 git clone https:///github.com/lukesmithxyz/st
@@ -9,4 +17,3 @@ cd st
 sudo make clean install
 cd ..
 mv * ../
-# install everything thx uwu
