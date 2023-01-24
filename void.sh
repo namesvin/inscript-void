358dd4ae8bb60c237cdb0cd37cbb56ae105b3258
v <temporarylife666@gmail.com>
2023-01-24 10:38:08 +0100

    asd

--- void.sh
+++ void.sh
@@ -68,7 +68,7 @@ if [ $? -eq 0 ]      # check exit code; if 0 EFI, else BIOS
 then
     echo "xbps-install -Suy xbps && xbps-install -uy && xbps-install -y base-system && xbps-remove -y base-voidstrap && xbps-install -y grub grub-x86_64-efi && grub-install --target=x86_64-efi --efi-directory=/boot && grub-mkconfig -o /boot/grub/grub.cfg && xbps-reconfigure -fa && passwd root" > /mnt/void/root/post.sh
 else
-    echo "xbps-install -Suy xbps && xbps-install -uy && xbps-install -y base-system && xbps-remove -y base-voidstrap && xbps-install -y grub && grub-install /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg && xbps-reconfigure -fa && passwd root" > /mnt/void/root/post.sh
+    echo "xbps-install -Suy xbps && xbps-install -uy && xbps-install -y base-system && xbps-remove -y base-voidstrap && xbps-install -y grub && grub-install ${DISK}1 && grub-mkconfig -o /boot/grub/grub.cfg && xbps-reconfigure -fa && passwd root" > /mnt/void/root/post.sh
 fi
 
 chmod u+x /mnt/void/root/post.sh
